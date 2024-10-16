248b225083086774101cd9ad3be0c620c48c3241
Fedmichard <francoisfedmichard@gmail.com>
2024-10-16 14:54:48 -0400

    Added stdlib.h for use of memory allocation funcs.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "test1.h"
 
 int main () {
+    void* ptr = malloc(6);
+    // void* arr = calloc();
+    printf(ptr);
+    free(ptr);
+    // realloc(ptr);
+
     printf("Hello World!\n");
     print("My Name is Fed!");
 
